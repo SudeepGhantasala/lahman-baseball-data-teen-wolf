49a6f1b9033c2b45823fd35db56a713651c162f7
SudeepGhantasala <sudeep.ghantasala@gmail.com>
2021-06-08 10:47:02 -0500

    question2final

--- scripts/Teen_Wolf_Q(2)_sudeep.sql
+++ scripts/Teen_Wolf_Q(2)_sudeep.sql
@@ -1,8 +1,14 @@
+
+--Find the name and height of the shortest player in the database. 
+--How many games did he play in? What is the name of the team for which he played?
+
 --question 2
-select playerid, namefirst ,namelast,(height), g_all, teamid
+select distinct namefirst ,namelast, height, g_all, name
 from people
 left join appearances
 using( playerid)
+left join teams
+using( teamid)
 where height = (select min(height)
 				from people)
 				
